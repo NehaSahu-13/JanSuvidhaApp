a895b90ceca92adc64a54c916c85f8a338b2045e
NehaSahu-13 <nsahu1477@gmail.com>
2025-05-28 22:15:25 +0530

    FileUpload

--- src/main/java/com/appbackend/Service/FileServiceImpl.java
+++ src/main/java/com/appbackend/Service/FileServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -17,40 +18,35 @@ import java.nio.file.StandardCopyOption;
 @Service
 public class FileServiceImpl implements FileService {
 
-    private final ResourceLoader resourceLoader;
-
-    public FileServiceImpl(ResourceLoader resourceLoader) {
-        this.resourceLoader = resourceLoader;
-    }
+   
 
     @Override
-    public String uploadFile(MultipartFile file) throws IOException{
-        try {
-           
-            File saveDir = new ClassPathResource("static/files").getFile();
-            if (!saveDir.exists()) {
-                saveDir.mkdirs(); 
-            }
-            Path path = Paths.get(saveDir.getAbsolutePath() + File.separator + file.getOriginalFilename());
-
-            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-
-            return file.getOriginalFilename();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "Error uploading file: " + e.getMessage();
+public String uploadFile(MultipartFile file) throws IOException {
+    try {
+        File saveDir = new File("uploads/files");
+        if (!saveDir.exists()) {
+            saveDir.mkdirs();
         }
-    }
 
-    @Override
-    public InputStream getResource(String path, String filename) {
-        try {
-           
-            Resource resource = resourceLoader.getResource("classpath:static/files/" + filename);
-            return resource.getInputStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+        Path path = Paths.get(saveDir.getAbsolutePath(), file.getOriginalFilename());
+        Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+        return file.getOriginalFilename();
+    } catch (IOException e) {
+        e.printStackTrace();
+        return "Error uploading file: " + e.getMessage();
     }
+}
+
+  @Override
+public InputStream getResource(String path, String filename) {
+    try {
+        Path filePath = Paths.get("uploads/files", filename);
+        return new FileInputStream(filePath.toFile());
+    } catch (IOException e) {
+        e.printStackTrace();
+        return null;
+    }
+}
+
 }
