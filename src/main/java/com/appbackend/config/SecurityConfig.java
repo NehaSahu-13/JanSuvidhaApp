fadd080d7300421318b435392a0d8b2adbab07e7
NehaSahu-13 <nsahu1477@gmail.com>
2025-05-30 08:29:08 +0530

    appcors

--- src/main/java/com/appbackend/config/SecurityConfig.java
+++ src/main/java/com/appbackend/config/SecurityConfig.java
@@ -81,10 +81,10 @@ public class SecurityConfig {
         .csrf(csrf -> csrf.disable())
         .cors(cors -> cors.configurationSource(request -> {
             CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(java.util.List.of("http://127.0.0.1:5500")); 
+            configuration.setAllowedOrigins(java.util.List.of("*")); 
             configuration.setAllowedMethods(java.util.List.of("POST", "GET", "OPTIONS", "DELETE", "PUT"));
             configuration.setAllowedHeaders(java.util.List.of("*"));
-            configuration.setAllowCredentials(true); 
+            configuration.setAllowCredentials(false); 
             return configuration;
         }))
         .authorizeHttpRequests(request -> request
